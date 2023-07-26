10013fe66bb3089023c5faeea61fbd11e7770c08
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-26 13:44:28 +0600

    card added

---
+++ src/components/BlogPost.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const BlogPost = () => {
+    return (
+      <div>
+        <div className="card w-80 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://roar.media/wp-content/uploads/2023/07/18.png"
+              alt="Thumbnail"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">তামিমের বিকল্প ভাবার সময়ই তো নেই!</h2>
+            <p>তামিম ইকবালকে ঘিরে গত কয়েকদিনে যা হলো, রায়হান রাফির হাতে এই চিত্রনাট্য পড়লে এ থেকে আরও একটা বক্স অফিস হিট সিনেমা বেরোত নিশ্চিত।</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">বিস্তারিত</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default BlogPost;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Appnav from '../components/Appnav';
+import BlogPost from '../components/BlogPost';
 
 const Layout = (props) => {
     return (
         <div>
             <Appnav/>
+            <BlogPost/>
             {props.children}
         </div>
     );
