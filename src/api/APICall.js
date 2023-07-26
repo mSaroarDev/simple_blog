90faed7554981b3f4e452f24fc06ccabbbea70cb
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-26 15:56:58 +0600

    up

--- src/api/APICall.js
+++ src/api/APICall.js
@@ -3,11 +3,22 @@ import axios from "axios";
 const baseUrl = 'https://basic-blog.teamrabbil.com/api';
 
 export async function postCatergory(){
-    const res = await axios.get(baseUrl + '/post-categories');
+    let res = await axios.get(baseUrl + '/post-categories');
 
     if(res.status === 200){
         return res.data;
     }else{
         return []
+    }
+}
+
+
+export async function newestPost(){
+    let res = await axios.get(baseUrl + '/post-newest');
+
+    if(res.status === 200){
+        return res.data;
+    }else{
+        return [];
     }
 }
