c2d90cbe10bc0d2d1765448c3ca038624eda1995
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-26 13:25:02 +0600

    shaddow added

--- src/components/Appnav.jsx
+++ src/components/Appnav.jsx
@@ -14,7 +14,7 @@ const Appnav = () => {
   }, []);
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 shadow">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
