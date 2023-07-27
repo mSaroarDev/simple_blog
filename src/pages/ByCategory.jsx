9a55a23e148d9b366854ba0bf4e6088ffe75ef03
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-07-27 11:40:08 +0600

    Update ByCategory.jsx

--- src/pages/ByCategory.jsx
+++ src/pages/ByCategory.jsx
@@ -1,29 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { postList } from '../api/APICall';
-import Loader from '../components/Loader';
-import Layout from '../layout/Layout';
-import BlogPost from './../components/BlogPost';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { postList } from "../api/APICall";
+import Loader from "../components/Loader";
+import Layout from "../layout/Layout";
+import BlogPost from "./../components/BlogPost";
 
 const ByCategory = () => {
+  const { categoryId } = useParams();
+  const [list, setList] = useState(null);
 
-    const {categoryId} = useParams();
-    const [list, setList] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let res = await postList(categoryId);
+      setList(res);
+    })();
+  }, [categoryId]);
 
-    useEffect(() => {
-        (async() => {
-            let res = await postList(categoryId);
-            setList(res);
-        })();
-    }, []);
-
-    return (
-        <Layout>
-            {
-                list === null ? <Loader /> : <BlogPost list={list} />
-            }
-        </Layout>
-    );
+  return (
+    <Layout>{list === null ? <Loader /> : <BlogPost list={list} />}</Layout>
+  );
 };
 
 export default ByCategory;
